2371dce308f9311e6e82961162db8aaab387b51a
zhangpc <zhangpc@seehoo.cn>
2022-05-14 19:19:48 +0800

    提交cocoapods版本的混合工程

---
+++ Native_project/Pods/Target Support Files/Pods-Native_project-Native_projectUITests/Pods-Native_project-Native_projectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Native_project_Native_projectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Native_project_Native_projectUITests
+@end
---
+++ Native_project/Pods/Target Support Files/Pods-Native_project/Pods-Native_project-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Native_project : NSObject
+@end
+@implementation PodsDummy_Pods_Native_project
+@end
